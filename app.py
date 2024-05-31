ec635f4b38923c7227e4ced52c8505996692b0fc
HARI_IITM <mech.hnath@gmail.com>
2024-05-31 22:29:41 +0530

    error fixed

--- app.py
+++ app.py
@@ -258,8 +258,8 @@ preprocessor = Pipeline(steps=[
 ])
 
 # read the training data
-path = r"C:\Users\Shivay\Desktop\flights-sagemaker-project\data\train.csv"
-train = pd.read_csv(path)
+# path = r"C:\\Users\\Shivay\\Desktop\\flights-sagemaker-project\\data\\train.csv"
+train = pd.read_csv(r"C:\Users\Shivay\Desktop\flights-sagemaker-project\data\train.csv")
 X_train = train.drop(columns="price")
 y_train = train.price.copy()
 
